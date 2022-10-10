9dc0682d44311b053343fdd2b2282efb01d26ced
George2365 <108607887+George2365@users.noreply.github.com>
2022-10-10 15:33:58 +0200

    Create Polymorphism.java

---
+++ Polymorphism.java
@@ -0,0 +1,66 @@
+public class PolyMain {
+    public static void main(String[] args) {
+
+        Computer pc = new Computer();
+        Phone phn = new Phone();
+        Tablet tbt = new Tablet();
+        Notebook ntb = new Notebook();
+        TV tv = new TV();
+
+        pc.start();
+        phn.start();
+        tbt.start();
+        ntb.start();
+        tv.start();
+    }
+}
+class Electronics {
+    //Superclass
+    public void start(){
+        System.out.println("starting device");
+    }
+}
+
+class TV extends Electronics{
+    //Child Class
+    @Override
+    public void start() {
+        System.out.print("TV: ");
+        super.start();
+    }
+}
+class Phone extends Electronics{
+    //Child Class
+    @Override
+    public void start() {
+        System.out.print("Phone: ");
+        super.start();
+    }
+}
+class Notebook extends Electronics{
+    //Child Class
+    @Override
+    public void start() {
+        System.out.print("Notebook: ");
+        super.start();
+    }
+}
+class Computer extends Electronics{
+    //Child Class
+    @Override
+    public void start() {
+        System.out.print("Computer: ");
+        super.start();
+    }
+}
+class Tablet extends Electronics {
+    //Child Class
+    @Override
+    public void start() {
+        System.out.print("Tablet: ");
+        super.start();
+    }
+}
+
+
+
